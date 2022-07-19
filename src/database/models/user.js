1ff7ddd57dc6f7d42015ee9f5cbacd0aa87b1890
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-07-19 16:15:35 -0300

    02: Creating user model

---
+++ src/database/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    underscored: true
+  })
+  return User;
+};
+
+module.exports = User
